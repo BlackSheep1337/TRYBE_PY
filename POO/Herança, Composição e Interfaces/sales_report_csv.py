d9c19089e494b78e23f6e8ebe6ea02b87e1b35a3
alepereira <naitwa-alexandre@gmail.com>
2022-03-29 19:13:11 -0300

    reports csv

--- POO/Herança, Composição e Interfaces/sales_report_csv.py
+++ POO/Herança, Composição e Interfaces/sales_report_csv.py
@@ -1,7 +1,7 @@
 from sales_report import SalesReport
 from csv import DictWriter
 
-class SalesReportCsv(SalesReport):
+class SalesReportCSV(SalesReport):
     def serialize(self):
         with open(self.export_file + ".csv", "w") as file:
             headers = ["Coluna 1", "Coluna 2", "Coluna 3"]
@@ -9,6 +9,10 @@ class SalesReportCsv(SalesReport):
             csv_writer.writeheader()
             for item in self.build():
                 csv_writer.writerow(item)
+    
+    def get_length(self):
+        return len(self.build())
 
-sales_report = SalesReportCsv('my_report')
+sales_report = SalesReportCSV('my_report')
 sales_report.serialize()
+print(sales_report.get_length())
